efd9ac5d1c6a1e7d70e400490514cd32d8ea81e8
Jérémy Lorette <jeremy.lorette@outlook.com>
2022-10-11 18:20:54 +0200

    add pages fav

--- src/features/favoris/components/FavoriList/FavoriList.jsx
+++ src/features/favoris/components/FavoriList/FavoriList.jsx
@@ -5,7 +5,7 @@ function FavoriList({ favoris, removeFavori }) {
   console.log(favoris);
   return (
     <div className="w-75 d-flex flex-row flex-wrap justify-content-center">
-      {favoris?.map((f, index) => (
+      {favoris.map((f, index) => (
         <FavoriElement
           key={f.title + index}
           favoris={f}
